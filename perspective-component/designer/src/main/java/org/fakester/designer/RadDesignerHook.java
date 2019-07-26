ee6204435a9c98a9273729b875042b2e94e72dcf
perryaj <perry@perryjone.es>
2019-07-25 17:07:34 -0700

    add reporting datasource designer ui

--- perspective-component/designer/src/main/java/org/fakester/designer/RadDesignerHook.java
+++ perspective-component/designer/src/main/java/org/fakester/designer/RadDesignerHook.java
@@ -1,11 +1,14 @@
 package org.fakester.designer;
 
+import com.inductiveautomation.ignition.common.BundleUtil;
 import com.inductiveautomation.ignition.common.licensing.LicenseState;
 import com.inductiveautomation.ignition.common.util.LoggerEx;
 import com.inductiveautomation.ignition.designer.model.AbstractDesignerModuleHook;
 import com.inductiveautomation.ignition.designer.model.DesignerContext;
 import com.inductiveautomation.perspective.designer.api.PerspectiveDesignerInterface;
+import com.inductiveautomation.reporting.designer.api.DesignerDataSourceRegistry;
 import org.fakester.common.component.display.Image;
+import org.fakester.designer.reporting.RestJsonDataConfigPanel;
 
 public class RadDesignerHook extends AbstractDesignerModuleHook {
 
@@ -17,8 +20,16 @@ public class RadDesignerHook extends AbstractDesignerModuleHook {
 
     @Override
     public void startup(DesignerContext context, LicenseState activationState) throws Exception {
-        PerspectiveDesignerInterface.getComponentRegistry(context)
-            .registerComponent(Image.DESCRIPTOR);
+        super.startup(context, activationState);
+
+        /* add our bundle to centralize strings and allow i18n support */
+        BundleUtil.get().addBundle("datasource", RadDesignerHook.class.getClassLoader(), "datasource");
+
+        // register perspective component
+        PerspectiveDesignerInterface.get(context).getDesignerComponentRegistry().registerComponent(Image.DESCRIPTOR);
+
+        // register report datasource ui
+        DesignerDataSourceRegistry.get(context).register(RestJsonDataConfigPanel.FACTORY);
     }
 
     @Override
